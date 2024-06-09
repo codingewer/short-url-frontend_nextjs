515c02613f5f8f0fda06eb5caa55b02980829ef6
codingewer <yatli5645@gmail.com>
2024-06-10 00:29:26 +0300

    some here

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -12,6 +12,7 @@ import Image from "next/image";
 
 function AboutUs() {
   const dispatch = useDispatch();
+  const success = useSelector((state) => state.settings.success);
   const sitedata = useSelector((state) => state.settings.data);
   useEffect(() => {
     dispatch(GetSiteDataBySiteName());
@@ -20,41 +21,45 @@ function AboutUs() {
     window.scrollTo(0, 0); // Sayfanın en üstüne kaydır
     Aos.init({ duration: 2000 });
   }, []);
-  return (
-    <>
-      <Helmet>
-        <title>Linkamon | Hakkımızda</title>
-        <meta name="description" content="Linkamon Hakkımızda." />
-        <meta name="keywords" content="hakkımızda, linkamon hakkında" />
-      </Helmet>
-      <TopBar />
-      <div data-aos="fade-up" className="contacus-page">
-        <div className="faq-banner">
-          <div className="home-banner">
-            <div className="home-banner-inner">
-              <div className="frame-parent">
-                <div className="link-shortener-parent">
-                  <div className="link-shortener">
-                    <h1 className="para-kazann">Hakkımızda</h1>
+  if (!success) {
+    return null;
+  } else {
+    return (
+      <>
+        <Helmet>
+          <title>Linkamon | Hakkımızda</title>
+          <meta name="description" content="Linkamon Hakkımızda." />
+          <meta name="keywords" content="hakkımızda, linkamon hakkında" />
+        </Helmet>
+        <TopBar />
+        <div data-aos="fade-up" className="contacus-page">
+          <div className="faq-banner">
+            <div className="home-banner">
+              <div className="home-banner-inner">
+                <div className="frame-parent">
+                  <div className="link-shortener-parent">
+                    <div className="link-shortener">
+                      <h1 className="para-kazann">Hakkımızda</h1>
+                    </div>
                   </div>
                 </div>
               </div>
+              <Image
+                className="undraw-link-shortener-mvf6-1-icon"
+                loading="lazy"
+                alt=""
+                src={bgbanner}
+              />
             </div>
-            <Image
-              className="undraw-link-shortener-mvf6-1-icon"
-              loading="lazy"
-              alt=""
-              src={bgbanner}
-            />
+          </div>
+          <div className="contactus-content">
+            <div dangerouslySetInnerHTML={{ __html: sitedata?.AboutUs }}></div>
           </div>
         </div>
-        <div className="contactus-content">
-          <div dangerouslySetInnerHTML={{ __html: sitedata?.AboutUs }}></div>
-        </div>
-      </div>
-      <Footer />
-    </>
-  );
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default AboutUs;
